3df1c0eb6469e7642c1cd872cde4f29e65bd2282
joohe71 <joohe71@gmail.com>
2022-06-07 22:51:45 +0900

    Add : 메인페이지 전체적인 구조 잡기

--- front/src/layout/Main.js
+++ front/src/layout/Main.js
@@ -1,6 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 const Main = () => {
-  return <div>main 영역</div>;
+  return (
+    <Div>
+      <div>서비스 대표적으로 소개 및 시작하기 버튼</div>
+      <div>사용자 후기</div>
+      <div>서비스 특징 1</div>
+      <div>서비스 특징 2</div>
+      <div>서비스 특징 3</div>
+    </Div>
+  );
 };
+
+const Div = styled.div`
+  border: 2px solid black;
+  width: 100vw;
+  // display: flex;
+  // flex-direction: column;
+
+  div {
+    height: 50vh;
+  }
+`;
 
 export default Main;
