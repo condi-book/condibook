e37ba27294a0b6b9c249dd5b6e72fb316967519b
joohe71 <joohe71@gmail.com>
2022-06-07 19:34:04 +0900

    Style : 전역 퍼튼 스타일 코드 추가

--- front/src/GlobalStyle.js
+++ front/src/GlobalStyle.js
@@ -1,10 +1,17 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  body{
+  body {
     padding: 0;
     margin: 0;
   };
+
+  button {
+    background:none;
+    border-radius: 3px;
+    padding: 10px;
+
+  }
   `;
 
 export default GlobalStyle;
