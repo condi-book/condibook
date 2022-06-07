ee0ddc4b921f6ba402eace3f0614650fc3a250e8
joohe71 <joohe71@gmail.com>
2022-06-07 16:55:40 +0900

    Add : 라우터 구조 추가

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,30 @@
+import React, { useReducer, createContext } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { loginReducer } from "./reducer";
+import Header from "./layout/Header";
+import Main from "./layout/Main";
+
+export const UserStateContext = createContext(null);
+export const DispatchContext = createContext(null);
+
 function App() {
-  return <div className="App"></div>;
+  // useReducer 훅을 통해 userState 상태와 dispatch함수를 생성함.
+  const [userState, dispatch] = useReducer(loginReducer, {
+    user: null,
+  });
+
+  return (
+    <DispatchContext.Provider value={dispatch}>
+      <UserStateContext.Provider value={userState}>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Main />} />
+          </Routes>
+        </Router>
+      </UserStateContext.Provider>
+    </DispatchContext.Provider>
+  );
 }
 
 export default App;
