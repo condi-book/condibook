f918fc332810bac3c93c85ab6597b6aee1a7dee0
joohe71 <joohe71@gmail.com>
2022-06-08 15:25:34 +0900

    Update : 구조 변경

--- front/src/layout/Main.tsx
+++ front/src/layout/Main.tsx
@@ -1,25 +1,29 @@
 import React from "react";
 import styled from "styled-components";
+import Carousel from "./carousel/Carousel";
+
 const Main = () => {
   return (
-    <Div>
-      <div>서비스 대표적으로 소개 및 시작하기 버튼</div>
-      <div>사용자 후기</div>
-      <div>서비스 특징 1</div>
-      <div>서비스 특징 2</div>
-      <div>서비스 특징 3</div>
-    </Div>
+    <Container>
+      <div className="part">서비스 대표적으로 소개 및 시작하기 버튼</div>
+      <div className="part">
+        사용자 후기
+        <Carousel />
+      </div>
+      <div className="part">서비스 특징 1</div>
+      <div className="part">서비스 특징 2</div>
+      <div className="part">서비스 특징 3</div>
+    </Container>
   );
 };
 
-const Div = styled.div`
+const Container = styled.div`
   border: 2px solid black;
   width: 100vw;
-  // display: flex;
-  // flex-direction: column;
 
-  div {
+  .part {
     height: 50vh;
+    border: 2px solid black;
   }
 `;
 
