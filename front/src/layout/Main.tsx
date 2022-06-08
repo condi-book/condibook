a88fa1676fada07efdd5f17641c2bbbfb5490c45
joohe71 <joohe71@gmail.com>
2022-06-08 20:49:36 +0900

    Style : 타이틀 여백 추가

--- front/src/layout/Main.tsx
+++ front/src/layout/Main.tsx
@@ -7,7 +7,7 @@ const Main = () => {
     <Container>
       <div className="part">서비스 대표적으로 소개 및 시작하기 버튼</div>
       <div className="part even ">
-        사용자 후기
+        <div className="title">사용자 후기</div>
         <Carousel />
       </div>
       <div className="part">서비스 특징 1</div>
@@ -18,15 +18,20 @@ const Main = () => {
 };
 
 const Container = styled.div`
-  border: 2px solid black;
   width: 100vw;
 
   .part {
-    height: 70vh;
-    border: 2px solid black;
+    height: 85vh;
   }
   .even {
     background: #f8f9fc;
+
+    .title {
+      font-weight: 700;
+      font-size: 42px;
+      text-align: center;
+      padding: 40px 0;
+    }
   }
 `;
 
