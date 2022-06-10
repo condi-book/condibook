0d93a176848e9867c60fbe5cac4297ef6e91026c
Hyejin Eom <hjeom.dev@gmail.com>
2022-06-10 21:18:13 +0900

    fix: 카카오 로그인의 토큰, 계정 API 호출 부분 서비스로 이동

--- back/src/mvp/user/userRouter.js
+++ back/src/mvp/user/userRouter.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import { userService } from "./userService";
-import axios from "axios";
 
 const userRouter = Router();
 
@@ -24,35 +23,12 @@ userRouter.post("/login/kakao", async (req, res, next) => {
     try {
         const { code } = req.body; // authorization code
 
-        // 코드로 토큰 발급
-        const responseToken = await axios.post(
-            "https://kauth.kakao.com/oauth/token",
-            new URLSearchParams({
-                grant_type: "authorization_code",
-                client_id: process.env.KAKAO_CLIENT_ID,
-                redirect_uri: process.env.CLIENT_URL + "/callback/login/kakao",
-                code: code,
-            }),
-        );
-        const token = responseToken.data.access_token;
+        const token = await userService.getKakaoToken(code);
 
-        // 토큰으로 사용자 정보 조회
-        const responseAccount = await axios
-            .get("https://kapi.kakao.com/v2/user/me", {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            })
-            .then((res) => {
-                return res.data;
-            });
+        const account = await userService.getKakaoAccount(token);
 
         // 로그인 또는 사용자 생성
-        const result = await userService.login({
-            nickname: responseAccount.properties.nickname,
-            email: responseAccount.kakao_account.email,
-            image_url: responseAccount.properties.profile_image,
-        });
+        const result = await userService.login(account);
 
         // 사용자 정보 + JWT 반환
         res.send(result);
--- back/src/mvp/user/userService.js
+++ back/src/mvp/user/userService.js
@@ -1,6 +1,6 @@
 import { User } from "../../db";
 import jwt from "jsonwebtoken";
-
+import axios from "axios";
 class userService {
     static async login({ nickname, email, image_url }) {
         // 사용자 조회
@@ -24,6 +24,42 @@ class userService {
             token: token,
         };
         return result;
+    }
+
+    static async getKakaoToken(code) {
+        // 코드로 토큰 발급
+        const token = await axios
+            .post(
+                "https://kauth.kakao.com/oauth/token",
+                new URLSearchParams({
+                    grant_type: "authorization_code",
+                    client_id: process.env.KAKAO_CLIENT_ID,
+                    redirect_uri:
+                        process.env.CLIENT_URL + "/callback/login/kakao",
+                    code: code,
+                }),
+            )
+            .then((res) => res.data.access_token);
+
+        return token;
+    }
+    static async getKakaoAccount(token) {
+        // 토큰으로 사용자 정보 조회
+        const account = await axios
+            .get("https://kapi.kakao.com/v2/user/me", {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+            .then((res) => {
+                return {
+                    nickname: res.data.properties.nickname,
+                    email: res.data.kakao_account.email,
+                    image_url: res.data.properties.profile_image,
+                };
+            });
+
+        return account;
     }
 }
 
