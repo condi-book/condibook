d086244827b24d23b3ea646486289302751ac4c8
joohe71 <joohe71@gmail.com>
2022-06-10 15:20:07 +0900

    Rename : 파일 이름 변경

--- front/src/mypage/MyPageBookMarkDetail.tsx
+++ front/src/mypage/MyPageBookMarkDetail.tsx
@@ -1,24 +1,27 @@
 import React from "react";
 import styled from "styled-components";
+import { MypageFavoritesProps } from "./MyPageFavorites";
 
-const MypageFavoritesDetail = () => {
+const MypageBookmarkDetail = (item: MypageFavoritesProps["item"]) => {
+  // const save:boolean = item.favorite
+
   return (
-    <Div>
+    <Div {...item}>
       <div className="top part">
         <div>
-          <img alt="북마크 이미지"></img>
+          <img src={item.image} alt="북마크 이미지"></img>
         </div>
         <div>
           <span className="pe-7s-more"></span>
         </div>
       </div>
       <div className="middle part">
-        <span>북마크 이름</span>
+        <span>{item.group}</span>
       </div>
       <div className="bottom part">
         <div>
           <span className="pe-7s-link"></span>
-          <span>개수</span>
+          <span>{item.link_num}</span>
         </div>
         <div>
           <span className="pe-7s-ribbon"></span>
@@ -47,6 +50,11 @@ const Div = styled.div`
   .pe-7s-more {
     transform: rotate(90deg);
   }
+
+  .pe-7s-ribbon {
+    color: ${(item: MypageFavoritesProps["item"]) =>
+      item.favorite === true ? "red" : "black"};
+  }
 `;
 
-export default MypageFavoritesDetail;
+export default MypageBookmarkDetail;
