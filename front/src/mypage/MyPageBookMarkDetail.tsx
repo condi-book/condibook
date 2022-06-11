2e216b2bdbe79de1e8a5e5dcf824f0dfc4268bc7
joohe71 <joohe71@gmail.com>
2022-06-12 00:41:36 +0900

    Modify : mypage bookmarkdetail

--- front/src/mypage/MyPageBookMarkDetail.tsx
+++ front/src/mypage/MyPageBookMarkDetail.tsx
@@ -5,24 +5,30 @@ import {
   Droppable,
   DropResult,
 } from "react-beautiful-dnd";
+import styled from "styled-components";
+import SideBar from "../layout/SideBar";
 
-const listItems = [
-  { id: "1", name: "link1" },
-  { id: "2", name: "link2" },
-  { id: "3", name: "link3" },
-  { id: "4", name: "link4" },
-];
+const listContent = {
+  title: "프론트엔드",
+  listItems: [
+    { id: "1", name: "link1" },
+    { id: "2", name: "link2" },
+    { id: "3", name: "link3" },
+    { id: "4", name: "link4" },
+  ],
+};
 
 // 드래그할 때 스타일
 const getItemStyle = (isDragging: boolean, draggableStyle: any) => ({
-  background: isDragging ? "purple" : "white",
-  color: isDragging ? "white" : "black",
-  border: "5px solid black",
-  margin: "10px",
+  color: "black",
+  border: isDragging ? "1px solid black" : "1px solid rgba(76, 76, 76, 0.1)",
+  fontSize: 30,
+  padding: "1%",
   ...draggableStyle,
 });
+
 const MypageBookmarkDetail = () => {
-  const [list, setList] = useState(listItems);
+  const [list, setList] = useState(listContent.listItems);
 
   const onDragEnd = (result: DropResult) => {
     const { source, destination } = result;
@@ -35,41 +41,97 @@ const MypageBookmarkDetail = () => {
 
     setList(items);
   };
+
+  const handleClick = () => {
+    alert("링크 추가하는 모달 나오기");
+  };
+
   return (
-    <div>
-      <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId="list">
-          {(provided) => (
-            <div
-              className="list"
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-            >
-              {list.map((item, index) => {
-                return (
-                  <Draggable key={item.id} draggableId={item.id} index={index}>
-                    {(provided, snapshot) => (
-                      <div
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        style={getItemStyle(
-                          snapshot.isDragging,
-                          provided.draggableProps.style,
-                        )}
-                      >
-                        {item.name}
+    <Div>
+      <SideBar />
+      <div className="detail-container">
+        <div className="list box">
+          <div>{listContent.title}</div>
+          <div className="add dnd-item" onClick={handleClick}>
+            <span className="pe-7s-plus"></span>
+          </div>
+          <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId="link-list">
+              {(provided) => (
+                <div
+                  className="link-list"
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                >
+                  {list.map((item, index) => {
+                    return (
+                      <div key={item.id} className="dnd-item">
+                        <Draggable
+                          key={item.id}
+                          draggableId={item.id}
+                          index={index}
+                        >
+                          {(provided, snapshot) => (
+                            <div
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              style={getItemStyle(
+                                snapshot.isDragging,
+                                provided.draggableProps.style,
+                              )}
+                            >
+                              {item.name}
+                            </div>
+                          )}
+                        </Draggable>
                       </div>
-                    )}
-                  </Draggable>
-                );
-              })}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
-    </div>
+                    );
+                  })}
+                </div>
+              )}
+            </Droppable>
+          </DragDropContext>
+        </div>
+        <div className="content box">링크내용</div>
+      </div>
+    </Div>
   );
 };
 
+const Div = styled.div`
+  display: flex;
+  flex-direction: row;
+  background: #f8f9fc;
+
+  .detail-container {
+    display: flex;
+    // justify-content: center;
+    width: 90vw;
+    border: 2px solid green;
+  }
+
+  .box {
+    width: 50%;
+    padding: 1%;
+  }
+
+  .add {
+    text-align: center;
+    margin: auto;
+    font-size: 30px;
+    border: 1px solid rgba(76, 76, 76, 0.1);
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+
+  .dnd-item {
+    margin: 3% 0;
+    &:hover {
+      border: solid 1px black;
+    }
+  }
+`;
 export default MypageBookmarkDetail;
