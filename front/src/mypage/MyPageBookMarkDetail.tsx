f34cd564fefe22ba187bf230bd6ed57a17c1e380
joohe71 <joohe71@gmail.com>
2022-06-11 21:54:59 +0900

    Add : 나의 북마크 디테일 페이지 drop&down 구현

--- front/src/mypage/MyPageBookMarkDetail.tsx
+++ front/src/mypage/MyPageBookMarkDetail.tsx
@@ -1,7 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DropResult,
+} from "react-beautiful-dnd";
 
+const listItems = [
+  { id: "1", name: "link1" },
+  { id: "2", name: "link2" },
+  { id: "3", name: "link3" },
+  { id: "4", name: "link4" },
+];
+
+// 드래그할 때 스타일
+const getItemStyle = (isDragging: boolean, draggableStyle: any) => ({
+  background: isDragging ? "purple" : "white",
+  color: isDragging ? "white" : "black",
+  border: "5px solid black",
+  margin: "10px",
+  ...draggableStyle,
+});
 const MypageBookmarkDetail = () => {
-  return <div></div>;
+  const [list, setList] = useState(listItems);
+
+  const onDragEnd = (result: DropResult) => {
+    const { source, destination } = result;
+    if (!destination) return;
+
+    // listItems 복제
+    const items = Array.from(list);
+    const [newOrder] = items.splice(source.index, 1);
+    items.splice(destination.index, 0, newOrder);
+
+    setList(items);
+  };
+  return (
+    <div>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="list">
+          {(provided) => (
+            <div
+              className="list"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {list.map((item, index) => {
+                return (
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(
+                          snapshot.isDragging,
+                          provided.draggableProps.style,
+                        )}
+                      >
+                        {item.name}
+                      </div>
+                    )}
+                  </Draggable>
+                );
+              })}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
 };
 
 export default MypageBookmarkDetail;
