f4bc4498f7dff764fbbda35f01683bc93fe871cb
joohe71 <joohe71@gmail.com>
2022-06-12 13:49:58 +0900

    Modify : 링크 박스 내부 수정

--- front/src/mypage/MyPageBookMarkDetail.tsx
+++ front/src/mypage/MyPageBookMarkDetail.tsx
@@ -14,26 +14,43 @@ const listContent = {
     {
       id: "1",
       title: "티스토리",
-      content: "",
+      image: "",
+      content: "내용을 입력해주세요",
       link: "https://tychejin.tistory.com/231",
     },
     {
       id: "2",
       title: "okayoon",
-      content: "",
+      image: "",
+      content: "내용을 입력해주세요",
       link: "https://okayoon.tistory.com/entry/%EC%95%84%EC%9D%B4%ED%94%84%EB%A0%88%EC%9E%84iframe",
     },
-    { id: "3", title: "nykim", content: "", link: "https://nykim.work/107" },
-    { id: "4", title: "티스토리", content: "", link: "link4" },
+    {
+      id: "3",
+      title: "nykim",
+      image: "",
+      content: "내용을 입력해주세요",
+      link: "https://nykim.work/107",
+    },
+    {
+      id: "4",
+      title: "티스토리",
+      image: "",
+      content: "내용을 입력해주세요",
+      link: "link4",
+    },
   ],
 };
 
 // 드래그할 때 스타일
 const getItemStyle = (isDragging: boolean, draggableStyle: any) => ({
   color: "black",
-  border: isDragging ? "1px solid black" : "1px solid rgba(76, 76, 76, 0.1)",
-  fontSize: 30,
-  padding: "1%",
+  border: isDragging && "2px solid rgba(76, 76, 76, 0.1)",
+  fontSize: 20,
+  background:
+    "linear-gradient(90deg, #12C2E9 19.08%, #C471ED 49.78%, #F64F59 78.71%)",
+  borderRadius: 10,
+  cursor: "default",
   ...draggableStyle,
 });
 
@@ -87,7 +104,7 @@ const MypageBookmarkDetail = () => {
                           index={index}
                         >
                           {(provided, snapshot) => (
-                            <div
+                            <DnDiv
                               ref={provided.innerRef}
                               {...provided.draggableProps}
                               {...provided.dragHandleProps}
@@ -96,8 +113,29 @@ const MypageBookmarkDetail = () => {
                                 provided.draggableProps.style,
                               )}
                             >
-                              {item.title}
-                            </div>
+                              <div className="dnd-item-element">
+                                <div>
+                                  <span className="pe-7s-menu" />
+                                </div>
+                                <div>
+                                  <img src={item.image} alt="이미지" />
+                                </div>
+                                <div>
+                                  <div>{item.title}</div>
+                                  <div>{item.content}</div>
+                                </div>
+                                <div>
+                                  <span
+                                    className="pe-7s-comment icon"
+                                    onClick={() => alert("댓글달기")}
+                                  />
+                                  <span
+                                    className="pe-7s-trash icon"
+                                    onClick={() => alert("삭제하기")}
+                                  />
+                                </div>
+                              </div>
+                            </DnDiv>
                           )}
                         </Draggable>
                       </div>
@@ -147,9 +185,36 @@ const Div = styled.div`
 
   .dnd-item {
     margin: 3% 0;
-    &:hover {
-      border: solid 1px black;
+
+    .dnd-item-element {
+      display: flex;
+      justify-content: space-between;
+      background: white;
+      border-radius: 10px;
+
+      span {
+        font-size: 30px;
+      }
+
+      .pe-7s-menu {
+        font-weight: bold;
+        cursor: all-scroll;
+      }
+
+      .icon {
+        cursor: pointer;
+      }
+
+      div {
+        margin: auto;
+      }
     }
+  }
+`;
+
+const DnDiv = styled.div`
+  &:hover {
+    border: 2px solid rgba(76, 76, 76, 0.1);
   }
 `;
 export default MypageBookmarkDetail;
