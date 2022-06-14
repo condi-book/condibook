fbd2b57518b6d978ba9e97f53e18e5abe694bea0
Hyejin Eom <hjeom.dev@gmail.com>
2022-06-14 16:37:07 +0900

    add: 내 폴더 조회 (GET /folders)

--- back/src/mvp/folder/folderRouter.js
+++ back/src/mvp/folder/folderRouter.js
@@ -1,23 +1,37 @@
 import { Router } from "express";
 import { folderService } from "./folderService";
 import { checkErrorMessage } from "../../middlewares/errorMiddleware";
+import { loginRequired } from "../../middlewares/loginRequired";
 
 const folderRouter = Router();
 
-folderRouter.post("", async (req, res, next) => {
-    try {
-        const { title, explanation } = req.body;
+// folderRouter.post("", async (req, res, next) => {
+//     try {
+//         const { title, explanation } = req.body;
 
-        const result = await folderService.createFolder({
-            title,
-            explanation,
-        });
+//         const result = await folderService.createFolder({
+//             title,
+//             explanation,
+//         });
+//         checkErrorMessage(result);
+
+//         res.status(201).send(result);
+//     } catch (e) {
+//         next(e);
+//     }
+// });
+
+folderRouter.get("", loginRequired, async (req, res, next) => {
+    try {
+        const id = req.currentUserId;
+
+        const result = await folderService.getMyFolders({ user_id: id });
+        console.log(result);
         checkErrorMessage(result);
 
-        res.status(201).send(result);
+        res.status(200).send(result);
     } catch (e) {
         next(e);
     }
 });
-
 export { folderRouter };
--- back/src/mvp/folder/folderService.js
+++ back/src/mvp/folder/folderService.js
@@ -14,6 +14,31 @@ class folderService {
 
         return folder;
     }
+
+    static async getMyFolders({ user_id }) {
+        try {
+            let folders = await Folder.findAll({
+                where: { user_id: user_id },
+            });
+
+            const result = folders.map((folder) => {
+                return {
+                    ...folder,
+                    favorites: folder.favorites == 0 ? true : false,
+                };
+            });
+            return result;
+        } catch (e) {
+            return {
+                errorMessage: getQueryResultMsg({
+                    result: true,
+                    expectation: false,
+                    entity: "폴더",
+                    queryType: "조회",
+                }),
+            };
+        }
+    }
 }
 
 export { folderService };
