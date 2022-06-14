952671b318cf979d60575e14a927299ee387c7ab
joohe71 <joohe71@gmail.com>
2022-06-14 17:16:53 +0900

    Style: 버튼 스타일 변경

--- front/src/auth/kakaoLoginBtn.tsx
+++ front/src/auth/kakaoLoginBtn.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { CLIENT_URL, KAKAO_CLIENT_ID } from "../config";
+import styled from "styled-components";
 
 const KakaoLoginBtn = () => {
   const KAKAO_REDIRECT_URI = CLIENT_URL + "/callback/login/kakao";
   const KAKAO_REQUEST_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}`;
   return (
-    <div>
+    <Div>
       <a href={KAKAO_REQUEST_URL} id="kakaoSignInDiv">
-        <img src={`static/img/kakaoButton.png`} />
+        <img width="90%" src={`static/img/kakaoButton.png`} />
       </a>
-    </div>
+    </Div>
   );
 };
 
+const Div = styled.div`
+  text-align: center;
+`;
 export default KakaoLoginBtn;
