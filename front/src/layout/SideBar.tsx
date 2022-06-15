83ac8ab4f8a6e3851ef9d403f4c7896d2c4ec006
joohe71 <joohe71@gmail.com>
2022-06-15 18:39:39 +0900

    Add : 경로 핸들러 함수 추가 및 적용

--- front/src/layout/SideBar.tsx
+++ front/src/layout/SideBar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 const iconList = [
@@ -11,10 +12,32 @@ const iconList = [
 ];
 
 const SideBar = () => {
+  const navigate = useNavigate();
+  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (e.currentTarget.id === "pe-7s-user") {
+      navigate("/profile");
+    }
+    if (e.currentTarget.id === "pe-7s-home") {
+      navigate("/");
+    }
+    if (e.currentTarget.id === "pe-7s-folder") {
+      navigate("/bookmark");
+    }
+    if (e.currentTarget.id === "pe-7s-global") {
+      navigate("/community");
+    }
+    if (e.currentTarget.id === "pe-7s-search") {
+      navigate("/search");
+    }
+    if (e.currentTarget.id === "pe-7s-config") {
+      navigate("/config");
+    }
+  };
+
   return (
     <Section>
       {iconList.map((item: string, index: number) => (
-        <div key={`icon-${index}`}>
+        <div key={`icon-${index}`} id={item} onClick={handleClick}>
           <span className={item}></span>
         </div>
       ))}
