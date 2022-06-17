d6665e036393e360d5aa7cbee635717ae24bc089
PrettyHan <imbel9830@gmail.com>
2022-06-17 13:41:50 +0900

    fix : board => post 네이밍 변경

--- back/src/mvp/attached/attachedService.js
+++ back/src/mvp/attached/attachedService.js
@@ -1,20 +1,20 @@
-import { User, Comment, Board } from "../../db";
+import { User, Comment, Post } from "../../db";
 
 class attachedService {
-    static async createComment({ content, user_id, board_id }) {
+    static async createComment({ content, user_id, post_id }) {
         if (!content) {
             const errorMessage = "댓글내용이 없습니다.";
             return { errorMessage };
         }
 
         const userinfo = await User.findOne({ where: { id: user_id } });
-        const board_fk_id = await Board.findOne({ where: { id: board_id } });
+        const post_fk_id = await Post.findOne({ where: { id: post_id } });
 
         const result = await Comment.create({
             content,
             author: user_id,
             author_name: userinfo.nickname,
-            board_id: board_fk_id.id,
+            post_id: post_fk_id.id,
         });
         if (!result) {
             const errorMessage = "해당 데이터가 없습니다.";
@@ -34,8 +34,8 @@ class attachedService {
         }
         return result;
     }
-    static async getCommentList({ board_id }) {
-        const result = Comment.findAll({ where: { board_id } });
+    static async getCommentList({ post_id }) {
+        const result = Comment.findAll({ where: { post_id } });
 
         if (!result) {
             const errorMessage = "해당 데이터가 없습니다.";
