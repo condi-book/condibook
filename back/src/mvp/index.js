d6147fad0864a695d49b4a570162eebe47b2f769
PrettyHan <imbel9830@gmail.com>
2022-06-17 13:38:04 +0900

    fix : board => post 네이밍 수정

--- back/src/mvp/index.js
+++ back/src/mvp/index.js
@@ -2,14 +2,14 @@ import { userRouter } from "./user/userRouter";
 import { websiteRouter } from "./website/websiteRouter";
 import { bookmarkRouter } from "./bookmark/bookmarkRouter";
 import { folderRouter } from "./folder/folderRouter";
-import { boardRouter } from "./board/boardRouter";
+import { postRouter } from "./post/postRouter";
 import { commentRouter } from "./comment/commentRouter";
 const indexRouter = (app) => {
     app.use("/user", userRouter);
     app.use("/website", websiteRouter);
     app.use("/bookmarks", bookmarkRouter);
     app.use("/folders", folderRouter);
-    app.use("/board", boardRouter);
+    app.use("/post", postRouter);
     app.use("/comment", commentRouter);
 };
 
