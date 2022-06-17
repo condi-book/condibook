de3529bdc02d408fe4beb424ae08ffcff683504a
PrettyHan <imbel9830@gmail.com>
2022-06-17 14:57:33 +0900

    add: like-api 좋아요 생성, 조회

--- back/src/mvp/index.js
+++ back/src/mvp/index.js
@@ -4,6 +4,7 @@ import { bookmarkRouter } from "./bookmark/bookmarkRouter";
 import { folderRouter } from "./folder/folderRouter";
 import { postRouter } from "./post/postRouter";
 import { commentRouter } from "./comment/commentRouter";
+import { likeRouter } from "./like/likeRouter";
 const indexRouter = (app) => {
     app.use("/user", userRouter);
     app.use("/website", websiteRouter);
@@ -11,6 +12,7 @@ const indexRouter = (app) => {
     app.use("/folders", folderRouter);
     app.use("/post", postRouter);
     app.use("/comment", commentRouter);
+    app.use("/like", likeRouter);
 };
 
 export { indexRouter };
