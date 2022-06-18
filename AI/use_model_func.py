7711f0d6824a07d5994d83ea6275d06ba98d6bed
seogudwns <38065726+seogudwns@users.noreply.github.com>
2022-06-18 10:26:16 +0900

    use_AI_model_func 주석으로 설명 추가

--- AI/use_model_func.py
+++ AI/use_model_func.py
@@ -17,39 +17,37 @@ def nouns_extractor(arr):
     if len(arr) == 0:
         return arr
     
-    arr = regex.findall(arr)
-    for i in range(len(arr)):
-        if arr[i] in del_arr:
-            arr[i] = ''
-    arr = ' '.join(arr)
-    
     reserverd_nouns = []
     text = ''
     
+    #명사는 넣고, 영어는 재분류를 위해 빼기.
     for i in okt.pos(arr):
         if i[1] == 'Noun':
             reserverd_nouns.append(i[0])
         elif i[1] == 'Alpha':
             text = text + i[0]+' '
     
+    # 영어가 있는 경우 실행되는 코드로 위와 똑같이 명사 분류.
     text1 = ''
     if len(text) != 0:
-        text1 = translator.translate(text1,dest = 'ko').text
+        text1 = translator.translate(text1, src = 'en', dest = 'ko').text
 
         for i in okt.pos(text1):
             if i[1] == 'Noun':
                 reserverd_nouns.append(i[0])
     
+    #불용어 제거 후 return.(중복 가능)
     nouns = [i for i in reserverd_nouns if i not in del_arr]
     return nouns
 
 # model2 : bookmark_extract func.
-
+# reserved_bookmark_list = [(title[i],1) for i in range(len(title_nouns))] + model.wv.most_similar(positive = title_nouns)
 def keywords_sum_similarity(reserved_bookmark_list,description_nouns):
-    # description이 없는 경우.
+    # description이 없는 경우.. title의 keyword는 있겠지..
     if len(description_nouns) == 0:
         return reserved_bookmark_list
     
+    # keyword_similarity_sum 구하고 return.
     keywords_ordered = ['']*len(reserved_bookmark_list)
     for i in range(len(reserved_bookmark_list)):
         keyword,per = reserved_bookmark_list[i]
@@ -75,6 +73,7 @@ def recommend_by_keyword(keywords,bookmark_list):
     #관심사 키워드 + bookmark된 키워드 리스트의 목록중 받아온 북마크에 있는 것만 뽑아주기. 
     x = model.wv.most_similar(positive = keywords) # len = 10
     
+    # 모든 것을 뽑는거는 애매하고 유사한 것중에서 3개 이하로 뽑기.(없으면 없는대로 보내기.)
     temp = []
     for i in x:
         if i in bookmark_list:
