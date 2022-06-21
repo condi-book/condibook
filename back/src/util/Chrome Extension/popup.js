906e9148953bb09cb607f0ae736c426d2f658f4e
joohe71 <joohe71@gmail.com>
2022-06-21 14:00:00 +0900

    Add : 링크 불러오는 로직 변경

--- back/src/util/Chrome Extension/popup.js
+++ back/src/util/Chrome Extension/popup.js
@@ -1,52 +1,48 @@
-
-function getCurrentTabUrl(callback) {  
+function getCurrentTabUrl(callback) {
     var queryInfo = {
-      active: true, 
-      currentWindow: true 
+        active: true,
+        currentWindow: true,
     };
-    
-    chrome.tabs.query(queryInfo, function(tabs) {
-      var tab = tabs[0]; 
-      var url = tab.url;
-      callback(url);
+
+    chrome.tabs.query(queryInfo, function (tabs) {
+        var tab = tabs[0];
+        var url = tab.url;
+        callback(url);
     });
-  }
+}
 
-  function renderURL(statusText) {
-    document.getElementById('status').textContent = statusText;
-  }
-  
-  document.addEventListener('DOMContentLoaded', function() {
+function renderURL(statusText) {
+    document.getElementById("status").textContent = statusText;
+}
 
+document.addEventListener("DOMContentLoaded", function () {
     // when click, get current page link
-  
-    var link = document.getElementById('getUrl');
 
-    link.addEventListener('click', function() {
-  
-      getCurrentTabUrl(function(url) {
+    // var link = document.getElementById('getUrl');
+
+    // link.addEventListener('click', function() {
+
+    getCurrentTabUrl(function (url) {
         renderURL(url);
-        const answer = url
+        const answer = url;
 
-        fetch('http://localhost:5001/website', {
-          method : "POST",
-          headers: {
-            "Content-Type" : "application/json",
-          },
-          mode: "cors",
-          body: JSON.stringify({
-            "url" : answer
-          })
-          }).then((res) => {
-            console.log(res,"성공")
-        }).catch((err) =>{
-            console.log(err)
+        fetch("http://localhost:5001/website", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            mode: "cors",
+            body: JSON.stringify({
+                url: answer,
+            }),
         })
-        
-      });
-      
-
+            .then((res) => {
+                console.log(res, "성공");
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     });
-  
-  
-  });
+});
+
+// });
