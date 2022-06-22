06826dd30b3e3d04ec5123fb9c8a2856e3191403
joohe71 <joohe71@gmail.com>
2022-06-23 01:57:32 +0900

    Style : 푸터 로고 추가 및 디자인 수정

--- front/src/layout/Footer.tsx
+++ front/src/layout/Footer.tsx
@@ -4,18 +4,28 @@ import styled from "styled-components";
 const Footer = () => {
   return (
     <Container>
-      <img alt="logo" />
-      <div>Copyright © 2022 CondiBook. All rights reserved.</div>
+      <img src="static/img/footer.png" alt="logo" />
+      <div className="description">
+        Copyright © 2022 CondiBook. All rights reserved.
+      </div>
     </Container>
   );
 };
 
 const Container = styled.footer`
-  height: 15vh;
+  height: 30vh;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
   align-items: center;
+  img {
+    padding: 3%;
+    width: 43%;
+    height: 80%;
+  }
+  .description {
+    font-size: 1.2vw;
+  }
 `;
 
 export default Footer;
