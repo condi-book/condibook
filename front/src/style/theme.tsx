e1609ca5a920bc8673c750ff6d8cf5f6d62168b2
joohe71 <joohe71@gmail.com>
2022-06-23 00:15:47 +0900

    Add : 필요 컬러변수 추가

--- front/src/style/theme.tsx
+++ front/src/style/theme.tsx
@@ -2,6 +2,9 @@ const mainColor: String = "linear-gradient(90deg, #17c0e9, #c96ddd, #f45162)";
 
 const profileBackground: String =
   "linear-gradient(135deg, #12c2e9, #c471ed, #f64f59)";
-const theme = { mainColor, profileBackground };
+
+const subBlackColor: String = "#111215";
+const subRedColor: String = "#f45162";
+const theme = { mainColor, profileBackground, subBlackColor, subRedColor };
 
 export default theme;
