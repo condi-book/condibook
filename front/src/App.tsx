b41b68440253c56d620246b03bbf5cbbef452bbd
joohe71 <joohe71@gmail.com>
2022-06-23 14:43:33 +0900

    Modify: 북마크 라우트 변경

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -69,7 +69,7 @@ const App: React.FC = () => {
                 <Route path="/bookmark" element={<Mypage />} />
                 <Route path="/login" element={<Login />} />
                 <Route
-                  path="/bookmark/:group"
+                  path="/:title/:linkId/"
                   element={<MypageBookmarkDetail />}
                 />
                 <Route path="/config" element={<Config />} />
