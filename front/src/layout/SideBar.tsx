490791d72ca6d4b9f9384f629e435cfeefc6c350
joohe71 <joohe71@gmail.com>
2022-06-27 01:12:21 +0900

    Add : GlobalAddBookmark 버튼 추가

--- front/src/layout/SideBar.tsx
+++ front/src/layout/SideBar.tsx
@@ -1,3 +1,4 @@
+import GlobalAddBookmarkButton from "GlobalAddBookMarkButton";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
@@ -58,6 +59,7 @@ const SideBar = () => {
           </div>
         ))}
       </Section>
+      <GlobalAddBookmarkButton />
       {show && <Profile data={data} handleApply={handleApply} />}
     </>
   );
