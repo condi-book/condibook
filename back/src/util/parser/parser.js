371035950e6bb13bb11dd4c1782606bf7aa53658
Hyejin Eom <hjeom.dev@gmail.com>
2022-06-28 20:27:19 +0900

    fix: parsers 버그 수정

--- back/src/util/parser/parser.js
+++ back/src/util/parser/parser.js
@@ -5,11 +5,11 @@ async function parsers(url) {
 
     const ogTitle = meta.og?.title || undefined;
     const ogDescription = meta.og?.description || undefined;
-    const ogImg = meta.og?.description || undefined;
+    const ogImg = meta.og?.image || undefined;
 
     const metaTitle = meta.meta?.title || undefined;
     const metaDescription = meta.meta?.description || undefined;
-    const metaImg = meta.meta?.description || undefined;
+    const metaImg = meta.twitter?.image || undefined;
 
     let title = ogTitle ?? metaTitle;
     let description = ogDescription ?? metaDescription;
