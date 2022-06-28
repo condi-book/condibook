6ab754796f2b2ac29fc3a1506212363a4efca77b
joohe71 <joohe71@gmail.com>
2022-06-28 14:07:10 +0900

    Add : alert 창 추가

---
+++ front/src/layout/Alert.tsx
@@ -0,0 +1,13 @@
+import Swal from "sweetalert2";
+
+export const Alert = Swal.mixin({
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timer: 1500,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener("mouseenter", Swal.stopTimer);
+    toast.addEventListener("mouseleave", Swal.resumeTimer);
+  },
+});
