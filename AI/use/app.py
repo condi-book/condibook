4a541ad8d4ed92a0aa91e78d787a22a93a4db7cb
seogudwns <38065726+seogudwns@users.noreply.github.com>
2022-06-29 19:09:57 +0900

    fix: meta_title이 없는 경우 우선 애러가 없이 처리되게..

--- AI/use/app.py
+++ AI/use/app.py
@@ -12,9 +12,11 @@ def home():
 @app.route('/translate', methods = ['POST'])
 def translate():
     req = request.get_json()
-    title = req['title']
-    if title == 'etcetcetc':
-        return make_response({'hashtags':'etc','category':'etc'}), 200
+    title = ''
+    if 'title' in req:
+        title = req['title']
+    else:
+        return make_response({'ErrorMessage':'meta_title의 부재.' ,'category':'etc'}), 400
 
     title_nouns = nouns_extractor(title)
 
