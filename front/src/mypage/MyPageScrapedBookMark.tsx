9d372b1e064cef03e2d775ee18dc2080d550d5d4
joohe71 <joohe71@gmail.com>
2022-06-30 19:28:37 +0900

    Add : 스크랩 북마크 디테일 구현

--- front/src/mypage/MyPageScrapedBookMark.tsx
+++ front/src/mypage/MyPageScrapedBookMark.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import * as Api from "../api";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import CalcData from "../community/tools/CalcDate";
 
 type StyleSize = {
   styleSize: () => number;
@@ -30,14 +31,25 @@ const MypageScrapedBookmark = () => {
       <div className="like-container">
         <div className="like-wrap">
           {likesData.map((v) => {
+            const createdTime = CalcData(new Date(v.createdAt));
             return (
-              <div
+              <LikeCard
                 className="like-item"
                 key={`likes-${v.id}`}
                 onClick={() => navigate(`/community/${v.post.id}`)}
               >
-                <div className="like-title">{v.post.title}</div>
-              </div>
+                <div className="like-middle">
+                  <div className="like-title like">{v.post.title}</div>
+                  <div className="like fonts">{createdTime}</div>
+                </div>
+                <div className="like-footer">
+                  <div className="fonts author">{v.post.author_name}</div>
+                  <div className="likes-box">
+                    <span className="pe-7s-like fonts"></span>
+                    <p className="fonts">{v.post.like_counts}</p>
+                  </div>
+                </div>
+              </LikeCard>
             );
           })}
         </div>
@@ -52,6 +64,10 @@ const Div = styled.div<StyleSize>`
   border: 1px solid red;
   height: 90%;
 
+  .pe-7s-like {
+    font-weight: bold;
+    color: ${({ theme }) => theme.subRedColor};
+  }
   .like-container {
     width: 100%;
     height: 100%;
@@ -83,6 +99,55 @@ const Div = styled.div<StyleSize>`
       &:hover {
         cursor: pointer;
         box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2), 0 6px 4px rgba(0, 0, 0, 0.2);
+      }
+    }
+  }
+`;
+
+const LikeCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .author {
+    font-weight: bold;
+  }
+
+  .fonts {
+    font-size: 0.9vw;
+  }
+
+  .like {
+    width: 100%;
+  }
+
+  .like-title {
+    font-size: 1.2vw;
+    font-weight: bold;
+    margin-bottom: 5px;
+  }
+
+  .like-middle {
+    width: 100%;
+    margin-top: 12%;
+  }
+  .like-footer {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    margin-bottom: 5%;
+
+    .likes-box {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      p {
+        margin: 0;
+        margin-left: 5px;
       }
     }
   }
