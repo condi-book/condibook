cf24614ed18ff35d9316c29bba6192c046040edd
PrettyHan <imbel9830@gmail.com>
2022-06-30 13:58:35 +0900

    add: 서치-api, 통합검색

--- back/src/mvp/search/searchRouter.js
+++ back/src/mvp/search/searchRouter.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { searchSerivce } from "./searchService";
+import { loginRequired } from "../../middlewares/loginRequired";
 const searchRouter = Router();
 
 searchRouter.get("/community", async (req, res, next) => {
@@ -8,19 +9,48 @@ searchRouter.get("/community", async (req, res, next) => {
         const pageNumber = req.query.pageNumber;
         const content = req.query.content;
         const type = req.query.type; // type = 0 only title, type = 1 title + content
-        const result = await searchSerivce.getPostByQuery({
+        const postInfo = await searchSerivce.getPostByQuery({
             query,
             pageNumber,
             content,
             type,
         });
-        if (result.errorMessage) {
-            throw new Error(result.errorMessage);
+        if (postInfo.errorMessage) {
+            throw new Error(postInfo.errorMessage);
         }
-        res.status(200).send(result);
+        res.status(200).send(postInfo);
     } catch (error) {
         next(error);
     }
 });
 
+searchRouter.get("/unified", loginRequired, async (req, res, next) => {
+    try {
+        const user_id = req.current.user_id;
+        const query = req.query.order;
+        const pageNumber = req.query.pageNumber;
+        const content = req.query.content;
+        const type = 1;
+        const postinfo = await searchSerivce.getPostByQuery({
+            query,
+            pageNumber,
+            content,
+            type,
+        });
+        if (postinfo.errorMessage) {
+            throw new Error(postinfo.errorMessage);
+        }
+        const folderinfo = await searchSerivce.getFolderByQuery({
+            user_id,
+            pageNumber,
+            content,
+        });
+        if (folderinfo.errorMessage) {
+            throw new Error(folderinfo.errorMessage);
+        }
+        res.status(200).send({ postinfo, folderinfo });
+    } catch (error) {
+        next(error);
+    }
+});
 export { searchRouter };
