9db396cd8a079d75a5c200bb93570b5ea8c3413c
joohe71 <joohe71@gmail.com>
2022-07-03 02:11:41 +0900

    Add : 로딩 1.5초 추가

--- chrome-extension/src/App.js
+++ chrome-extension/src/App.js
@@ -1,5 +1,6 @@
 import PopUp from "./PopUp";
 import Success from "./Success";
+import { Suspense } from "react";
 
 import { useEffect, useState } from "react";
 import LoginButton from "./LoginButton";
@@ -56,98 +57,105 @@ const App = () => {
   };
 
   useEffect(() => {
-    chrome.cookies
-      .get({ url: "http://kdt-ai4-team14.elicecoding.com/", name: "userToken" })
-      .then(async (res) => {
-        const cookies = res.value;
-        setCookie(cookies);
-        await console.log(res.value);
-        await getCurrentTabUrl(function (url) {
-          setUrl(url);
-          const answer = url;
-          console.log("링크 가져오는 중");
-          console.log("링크 값", answer);
-          console.log(cookies);
-          fetch("http://kdt-ai4-team14.elicecoding.com:5001/websites", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${cookies}`,
-            },
-            mode: "cors",
-            body: JSON.stringify({
-              url: answer,
-            }),
-          })
-            .then((res) => res.json())
-            .then(async (data) => {
-              setTitle(data.website.meta_title);
-              setId(data.website.id);
-              console.log(data);
-              const copied = data;
-              const copiedList = copied.folders.map((item) => item.title);
-              await console.log("copiedlist", copiedList);
-              await copied.folders.push({ title: "직접입력", id: 0 });
-              if (copiedList.includes(copied.category.category)) {
-                console.log(
-                  copied.folders.filter(
-                    (item) => item.title === copied.category.category
-                  )[0]
-                );
-                const folderList = copied.folders.filter(
-                  (item) => item.title !== copied.category.category
-                );
-                folderList.unshift(
-                  copied.folders.filter(
-                    (item) => item.title === copied.category.category
-                  )[0]
-                );
-                const realData = { ...copied, folders: folderList };
-
-                console.log(realData);
-                setFolder(copied.category.category);
-                setData(realData);
-              } else {
-                copied.folders.unshift({
-                  title: data.category.category,
-                  id: null,
-                });
-                setData(copied);
-                setFolder(copied.category.category);
-              }
+    setTimeout(function () {
+      chrome.cookies
+        .get({
+          url: "http://kdt-ai4-team14.elicecoding.com/",
+          name: "userToken",
+        })
+        .then(async (res) => {
+          const cookies = res.value;
+          setCookie(cookies);
+          await console.log(res.value);
+          await getCurrentTabUrl(function (url) {
+            setUrl(url);
+            const answer = url;
+            console.log("링크 가져오는 중");
+            console.log("링크 값", answer);
+            console.log(cookies);
+            fetch("http://kdt-ai4-team14.elicecoding.com:5001/websites", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${cookies}`,
+              },
+              mode: "cors",
+              body: JSON.stringify({
+                url: answer,
+              }),
             })
-            .catch((err) => {
-              console.log(err.message);
-              console.log("에러났어요");
-              setStatus("NOT");
-            });
-        });
+              .then((res) => res.json())
+              .then(async (data) => {
+                setTitle(data.website.meta_title);
+                setId(data.website.id);
+                console.log(data);
+                const copied = data;
+                const copiedList = copied.folders.map((item) => item.title);
+                await console.log("copiedlist", copiedList);
+                await copied.folders.push({ title: "직접입력", id: 0 });
+                if (copiedList.includes(copied.category.category)) {
+                  console.log(
+                    copied.folders.filter(
+                      (item) => item.title === copied.category.category
+                    )[0]
+                  );
+                  const folderList = copied.folders.filter(
+                    (item) => item.title !== copied.category.category
+                  );
+                  folderList.unshift(
+                    copied.folders.filter(
+                      (item) => item.title === copied.category.category
+                    )[0]
+                  );
+                  const realData = { ...copied, folders: folderList };
 
-        await setStatus("SUCCESS");
-      })
-      .catch((err) => setStatus("FAIL"));
+                  console.log(realData);
+                  setFolder(copied.category.category);
+                  setData(realData);
+                } else {
+                  copied.folders.unshift({
+                    title: data.category.category,
+                    id: null,
+                  });
+                  setData(copied);
+                  setFolder(copied.category.category);
+                }
+              })
+              .catch((err) => {
+                console.log(err.message);
+                console.log("에러났어요");
+                setStatus("NOT");
+              });
+          });
+
+          await setStatus("SUCCESS");
+        })
+        .catch((err) => setStatus("FAIL"));
+    }, 1500);
   }, []);
 
   if (status === "SUCCESS") {
     return (
       <>
-        {!success ? (
-          <PopUp
-            handlePage={handlePage}
-            url={url}
-            data={data}
-            title={title}
-            cookie={cookie}
-            id={id}
-            folder={folder}
-            handleFolderChange={handleFolderChange}
-            handleInputChange={handleInputChange}
-            input={input}
-            inputValue={inputValue}
-          />
-        ) : (
-          <Success folder={folder} inputValue={inputValue} data={data} />
-        )}
+        <Suspense fallback={<Loading />}>
+          {!success ? (
+            <PopUp
+              handlePage={handlePage}
+              url={url}
+              data={data}
+              title={title}
+              cookie={cookie}
+              id={id}
+              folder={folder}
+              handleFolderChange={handleFolderChange}
+              handleInputChange={handleInputChange}
+              input={input}
+              inputValue={inputValue}
+            />
+          ) : (
+            <Success folder={folder} inputValue={inputValue} data={data} />
+          )}
+        </Suspense>
       </>
     );
   } else if (status === "FAIL") {
