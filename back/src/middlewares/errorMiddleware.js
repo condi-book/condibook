afaf58ffa06958d59bb91dd019eebf5619ac520c
Hyejin Eom <hjeom.dev@gmail.com>
2022-07-12 00:22:42 +0900

    delete: serverErrorMiddleware

--- back/src/middlewares/errorMiddleware.js
+++ back/src/middlewares/errorMiddleware.js
@@ -4,16 +4,10 @@ const errorMiddleware = (error, req, res, next) => {
     res.status(400).send(error.message);
 };
 
-const serverErrorMiddleware = (error, req, res, next) => {
-    // 터미널에 마젠타색으로 출력됨.
-    console.log("\x1b[35m%s\x1b[0m", error);
-    res.status(500).send(error.message);
-};
-
 const checkErrorMessage = (result) => {
     if (result.errorMessage) {
         throw Error(result.errorMessage);
     }
 };
 
-export { errorMiddleware, serverErrorMiddleware, checkErrorMessage };
+export { errorMiddleware, checkErrorMessage };
