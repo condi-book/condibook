787e22b50261401815dc085f6cb627bf102a63cf
PrettyHan <imbel9830@gmail.com>
2022-07-25 21:07:45 +0900

    modify: elasticSearch 주석처리

--- back/src/mvp/search/searchRouter.js
+++ back/src/mvp/search/searchRouter.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { searchSerivce } from "./searchService";
 import { loginRequired } from "../../middlewares/loginRequired";
 import { teamService } from "../team/teamService";
-import { elasticSearch } from "../../db/elasticsearch";
+// import { elasticSearch } from "../../db/elasticsearch";
 const searchRouter = Router();
 
 searchRouter.get("/community", async (req, res, next) => {
@@ -11,25 +11,22 @@ searchRouter.get("/community", async (req, res, next) => {
         const pageNumber = req.query.pageNumber ?? 1;
         const content = req.query.content;
         const type = req.query.type ?? 1; // type = 0 only title, type = 1 title + content
-        let info = null;
-        info = await elasticSearch.searchPost({ content, pageNumber });
-        const data = info.hits.hits;
-        const data2 = data.map((v) => {
-            return v._source;
+        // let info = null;
+        // info = await elasticSearch.searchPost({ content, pageNumber });
+        // const data = info.hits.hits;
+        // const data2 = data.map((v) => {
+        //     return v._source;
+        // });
+        const result = await searchSerivce.getPostByQuery({
+            query,
+            pageNumber,
+            content,
+            type,
         });
-        if (data2 == null && data2 == "") {
-            info = await searchSerivce.getPostByQuery({
-                query,
-                pageNumber,
-                content,
-                type,
-            });
-            if (info.errorMessage) {
-                throw new Error(info.errorMessage);
-            }
-            res.status(200).send(info);
+        if (result.errorMessage) {
+            throw new Error(result.errorMessage);
         }
-        res.status(200).send(data2);
+        res.status(200).send(result);
     } catch (error) {
         next(error);
     }
